42d979291c6ab95e3c637891ba2f1c2686bc25d3
BehroodRoozdar <b.roozdar@kian.digital>
2021-11-26 14:04:29 +0330

    setup project and config base project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Lists from './pages/Lists';
+import Home from './pages/Home';
+import Layout from './Components/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/lists" element={<Lists />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </Layout>
   );
 }
 
